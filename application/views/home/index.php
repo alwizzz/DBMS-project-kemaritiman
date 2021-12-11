09e9ae17078e7722a9c95e0d2796d0a028d63a87
alwizzz <alwiza21@gmail.com>
2021-12-11 23:43:27 +0700

    tambah tpi di navbar, refine hyperlinks

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -12,37 +12,37 @@
             </p>
             <h4>Kemaritiman</h4>
                 <ul>
-                    <li><a href=<?= site_url('Laut'); ?>>Laut</a></li>
-                    <li><a href=<?= site_url('Teluk'); ?>>Teluk</a></li>
-                    <li><a href=<?= site_url('Selat'); ?>>Selat</a></li>
-                    <li><a href=<?= site_url('Palung'); ?>>Palung</a></li>
-                    <li><a href=<?= site_url('Pantai'); ?>>Pantai</a></li>
-                    <li><a href=<?= site_url('GunungLaut'); ?>>Gunung Laut</a></li>
-                    <li><a href=<?= site_url('Pulau'); ?>>Pulau</a></li>
-                    <li><a href=<?= site_url('Provinsi'); ?>>Provinsi</a></li>
-                    <li><a href=<?= site_url('KotaKabupaten'); ?>>Kota / Kabupaten</a></li>
-                    <li><a href=<?= site_url('HutanMangrove'); ?>>Hutan Mangrove</a></li>
+                    <li><a href=<?= site_url('laut'); ?>>Laut</a></li>
+                    <li><a href=<?= site_url('teluk'); ?>>Teluk</a></li>
+                    <li><a href=<?= site_url('selat'); ?>>Selat</a></li>
+                    <li><a href=<?= site_url('palung'); ?>>Palung</a></li>
+                    <li><a href=<?= site_url('pantai'); ?>>Pantai</a></li>
+                    <li><a href=<?= site_url('gununglaut'); ?>>Gunung Laut</a></li>
+                    <li><a href=<?= site_url('pulau'); ?>>Pulau</a></li>
+                    <li><a href=<?= site_url('provinsi'); ?>>Provinsi</a></li>
+                    <li><a href=<?= site_url('kotakabupaten'); ?>>Kota / Kabupaten</a></li>
+                    <li><a href=<?= site_url('hutanmangrove'); ?>>Hutan Mangrove</a></li>
                 </ul>
             <h4>Pelayaran</h4>
                 <ul>
-                    <li><a href=<?= site_url('Pelabuhan'); ?>>Pelabuhan</a></li>
-                    <li><a href=<?= site_url('TransportasiLaut'); ?>>Transportasi Laut</a></li>
-                    <li><a href=<?= site_url('Kapal'); ?>>Kapal</a></li>
+                    <li><a href=<?= site_url('pelabuhan'); ?>>Pelabuhan</a></li>
+                    <li><a href=<?= site_url('transportasilaut'); ?>>Transportasi Laut</a></li>
+                    <li><a href=<?= site_url('kapal'); ?>>Kapal</a></li>
                 </ul>
             <h4>Perikanan</h4>
                 <ul>
-                    <li><a href=<?= site_url('Ekstraktif'); ?>>Ekstraktif</a></li>
-                    <li><a href=<?= site_url('AktivitasPenangkapanIkan'); ?>>Aktivitas Penangkapan Ikan</a></li>
+                    <li><a href=<?= site_url('ekstraktif'); ?>>Ekstraktif</a></li>
+                    <li><a href=<?= site_url('aktivitaspenangkapanikan'); ?>>Aktivitas Penangkapan Ikan</a></li>
                 </ul>
             <h4>Penambangan Laut</h4>
                 <ul>
-                    <li><a href=<?= site_url('Mineral'); ?>>Mineral</a></li>
-                    <li><a href=<?= site_url('PenambanganBawahLaut'); ?>>Penambangan Bawah Laut</a></li>
+                    <li><a href=<?= site_url('mineral'); ?>>Mineral</a></li>
+                    <li><a href=<?= site_url('penambanganbawahlaut'); ?>>Penambangan Bawah Laut</a></li>
                 </ul>
             <h4>Wisata Laut</h4>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <li><a href=<?= site_url('ObjekWisata'); ?>>Objek Wisata</a></li>
-                    <li><a href=<?= site_url('TamanLaut'); ?>>Taman Laut</a></li>
+                    <li><a href=<?= site_url('objekwisata'); ?>>Objek Wisata</a></li>
+                    <li><a href=<?= site_url('tamanlaut'); ?>>Taman Laut</a></li>
                 </ul>
         </div>
     </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -26,16 +26,16 @@
                         Kemaritiman
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href=<?= site_url('Laut'); ?>>Laut</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Teluk'); ?>>Teluk</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Selat'); ?>>Selat</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Palung'); ?>>Palung</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Pantai'); ?>>Pantai</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('GunungLaut'); ?>>Gunung Laut</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Pulau'); ?>>Pulau</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Provinsi'); ?>>Provinsi</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('KotaKabupaten'); ?>>Kota / Kabupaten</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('HutanMangrove'); ?>>Hutan Mangrove</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('laut'); ?>>Laut</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('teluk'); ?>>Teluk</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('selat'); ?>>Selat</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('palung'); ?>>Palung</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('pantai'); ?>>Pantai</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('gununglaut'); ?>>Gunung Laut</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('pulau'); ?>>Pulau</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('provinsi'); ?>>Provinsi</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('kotakabupaten'); ?>>Kota / Kabupaten</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('hutanmangrove'); ?>>Hutan Mangrove</a></li>
                     </ul>
                 </li>
             </ul>
@@ -47,9 +47,9 @@
                         Pelayaran
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href=<?= site_url('Pelabuhan'); ?>>Pelabuhan</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('TransportasiLaut'); ?>>Transportasi Laut</a></li>
-                        <li><a class="dropdown-item" href=<?= site_url('Kapal'); ?>>Kapal</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('pelabuhan'); ?>>Pelabuhan</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('transportasilaut'); ?>>Transportasi Laut</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('kapal'); ?>>Kapal</a></li>
                     </ul>
                     </li>
                   </ul>
@@ -61,8 +61,9 @@
                           Perikanan
                       </a>
                       <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                          <li><a class="dropdown-item" href=<?= site_url('Ekstraktif'); ?>>Ekstraktif</a></li>
-                          <li><a class="dropdown-item" href=<?= site_url('AktivitasPenangkapanIkan'); ?>>Aktivitas Penangkapan Ikan</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('ekstraktif'); ?>>Ekstraktif</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('tempatpelelanganikan'); ?>>Tempat Pelelangan Ikan</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('aktivitaspenangkapanikan'); ?>>Aktivitas Penangkapan Ikan</a></li>
                       </ul>
                       </li>
                     </ul>
@@ -74,8 +75,8 @@
                             Penambangan Laut
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                          <li><a class="dropdown-item" href=<?= site_url('Mineral'); ?>>Mineral</a></li>
-                          <li><a class="dropdown-item" href=<?= site_url('PenambanganBawahLaut'); ?>>Penambangan Bawah Laut</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('mineral'); ?>>Mineral</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('penambanganbawahlaut'); ?>>Penambangan Bawah Laut</a></li>
                         </ul>
                       </li>
                     </ul>
@@ -87,8 +88,8 @@
                             Wisata Laut
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href=<?= site_url('ObjekWisata'); ?>>Objek Wisata</a></li>
-                            <li><a class="dropdown-item" href=<?= site_url('TamanLaut'); ?>>Taman Laut</a></li>
+                            <li><a class="dropdown-item" href=<?= site_url('objekwisata'); ?>>Objek Wisata</a></li>
+                            <li><a class="dropdown-item" href=<?= site_url('tamanlaut'); ?>>Taman Laut</a></li>
                     </ul>
                 </li>
             </ul>
