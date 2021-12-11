f5c27ddfc685a8966f864b42cd3e700f2899b8d5
alwizzz <alwiza21@gmail.com>
2021-12-11 23:14:19 +0700

    Kelarin main page, refine navbar

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,49 @@
-
-    <h1>Main Menu</h1>
-
+<div class="container">
+    <br>
+    <div class="card">
+        <div class="card-body">
+            <h1>One Stop Service Kemaritiman</h1>
+            <p>
+            Database ini membahas tentang kemaritiman di Indonesia  sebagai fokus utama dan 
+            aspek-aspek lain yang berkaitan dengan kemaritiman seperti pelayaran, 
+            perikanan, penambangan laut, serta wisata laut sebagai informasi tambahan. 
+            Database ini digunakan sebagai penyedia informasi kemaritiman di Indonesia dalam 
+            satu tempat atau sering disebut sebagai one-stop service.
+            </p>
+            <h4>Kemaritiman</h4>
+                <ul>
+                    <li><a href=<?= site_url('Laut'); ?>>Laut</a></li>
+                    <li><a href=<?= site_url('Teluk'); ?>>Teluk</a></li>
+                    <li><a href=<?= site_url('Selat'); ?>>Selat</a></li>
+                    <li><a href=<?= site_url('Palung'); ?>>Palung</a></li>
+                    <li><a href=<?= site_url('Pantai'); ?>>Pantai</a></li>
+                    <li><a href=<?= site_url('GunungLaut'); ?>>Gunung Laut</a></li>
+                    <li><a href=<?= site_url('Pulau'); ?>>Pulau</a></li>
+                    <li><a href=<?= site_url('Provinsi'); ?>>Provinsi</a></li>
+                    <li><a href=<?= site_url('KotaKabupaten'); ?>>Kota / Kabupaten</a></li>
+                    <li><a href=<?= site_url('HutanMangrove'); ?>>Hutan Mangrove</a></li>
+                </ul>
+            <h4>Pelayaran</h4>
+                <ul>
+                    <li><a href=<?= site_url('Pelabuhan'); ?>>Pelabuhan</a></li>
+                    <li><a href=<?= site_url('TransportasiLaut'); ?>>Transportasi Laut</a></li>
+                    <li><a href=<?= site_url('Kapal'); ?>>Kapal</a></li>
+                </ul>
+            <h4>Perikanan</h4>
+                <ul>
+                    <li><a href=<?= site_url('Ekstraktif'); ?>>Ekstraktif</a></li>
+                    <li><a href=<?= site_url('AktivitasPenangkapanIkan'); ?>>Aktivitas Penangkapan Ikan</a></li>
+                </ul>
+            <h4>Penambangan Laut</h4>
+                <ul>
+                    <li><a href=<?= site_url('Mineral'); ?>>Mineral</a></li>
+                    <li><a href=<?= site_url('PenambanganBawahLaut'); ?>>Penambangan Bawah Laut</a></li>
+                </ul>
+            <h4>Wisata Laut</h4>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a href=<?= site_url('ObjekWisata'); ?>>Objek Wisata</a></li>
+                    <li><a href=<?= site_url('TamanLaut'); ?>>Taman Laut</a></li>
+                </ul>
+        </div>
+    </div>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,19 +12,86 @@
   </head>
   <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      
-  <div class="container">
-    <a class="navbar-brand" href="#">One Stop Service Kemaritiman</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Laut</a>
-            <a class="nav-link" href="#">Ekstraktif</a>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href=<?= site_url(); ?>>One Stop Service Kemaritiman</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Kemaritiman
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href=<?= site_url('Laut'); ?>>Laut</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Teluk'); ?>>Teluk</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Selat'); ?>>Selat</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Palung'); ?>>Palung</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Pantai'); ?>>Pantai</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('GunungLaut'); ?>>Gunung Laut</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Pulau'); ?>>Pulau</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Provinsi'); ?>>Provinsi</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('KotaKabupaten'); ?>>Kota / Kabupaten</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('HutanMangrove'); ?>>Hutan Mangrove</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pelayaran
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href=<?= site_url('Pelabuhan'); ?>>Pelabuhan</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('TransportasiLaut'); ?>>Transportasi Laut</a></li>
+                        <li><a class="dropdown-item" href=<?= site_url('Kapal'); ?>>Kapal</a></li>
+                    </ul>
+                    </li>
+                  </ul>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Perikanan
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <li><a class="dropdown-item" href=<?= site_url('Ekstraktif'); ?>>Ekstraktif</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('AktivitasPenangkapanIkan'); ?>>Aktivitas Penangkapan Ikan</a></li>
+                      </ul>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Penambangan Laut
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <li><a class="dropdown-item" href=<?= site_url('Mineral'); ?>>Mineral</a></li>
+                          <li><a class="dropdown-item" href=<?= site_url('PenambanganBawahLaut'); ?>>Penambangan Bawah Laut</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Wisata Laut
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href=<?= site_url('ObjekWisata'); ?>>Objek Wisata</a></li>
+                            <li><a class="dropdown-item" href=<?= site_url('TamanLaut'); ?>>Taman Laut</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
-  </div>
 </nav>
